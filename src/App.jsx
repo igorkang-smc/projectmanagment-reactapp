66f75cb12e06567ffca3a3b2150ffded6ff738a2
roginak <ikanv13@gmail.com>
2023-01-11 13:53:01 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Board from './pages/Board/Board.jsx'
+import Header from './components/Header/Header.jsx';
+
+function App() {
+    return (
+        <div className='App'>
+            <Header />
+            <Board />
+        </div>
+    );
+}
+
+export default App;
